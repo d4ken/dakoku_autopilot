ac4b17ec6fa5d5c602de103a9b78be917dbbaccc
d4ken <d-shikencho@techqiao.co.jp>
2024-05-20 18:10:23 +0900

    最終打刻日時をlocalstorageに保存

--- frontend/js/onLoad.js
+++ frontend/js/onLoad.js
@@ -1,5 +1,7 @@
 // 画面読み込み時に入力情報を読み込み
 window.onload = function () {
+    if (localStorage.getItem('last_timestamp'))
+        document.getElementById("last_timestamp").textContent = localStorage.getItem('last_timestamp')
     if (localStorage.getItem('username'))
         document.getElementById("username").value = localStorage.getItem('username')
     if (localStorage.getItem('password'))
