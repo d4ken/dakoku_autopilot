5672d8603e222b2e06eff74d2843b776f99b0de1
d4ken <d-shikencho@techqiao.co.jp>
2024-05-20 19:41:09 +0900

    最終打刻時刻の表示修正

--- frontend/js/onLoad.js
+++ frontend/js/onLoad.js
@@ -1,9 +1,10 @@
 // 画面読み込み時に入力情報を読み込み
 window.onload = function () {
-    if (localStorage.getItem('last_timestamp'))
-        document.getElementById("last_timestamp").textContent = localStorage.getItem('last_timestamp')
     if (localStorage.getItem('username'))
         document.getElementById("username").value = localStorage.getItem('username')
     if (localStorage.getItem('password'))
         document.getElementById("password").value = localStorage.getItem('password')
+    if (localStorage.getItem('last_timestamp'))
+        document.getElementById("last_timestamp").textContent = `最終打刻日時: ${localStorage.getItem('last_timestamp')}`
+
 }
