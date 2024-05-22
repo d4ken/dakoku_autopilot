f98e83866f8735d94e1499fa8b3f044afddb57cf
d4ken <d-shikencho@techqiao.co.jp>
2024-05-22 17:46:29 +0900

    出勤、退勤打刻処理を分離

--- frontend/js/attendance.js
+++ frontend/js/attendance.js
@@ -1,70 +1,73 @@
-let dakokuBtn;
+let attendanceBtn;
+let leavingBtn;
+
 const dakokuStatus = Object.freeze({
     attend: 'attend',
     leave: 'leave',
     none: 'none'
 })
 
-// 打刻ボタンクリック時処理
-const onClickDakoku = async () => {
+const attendanceDakoku = async () => {
     // ボタン連打防止処理
-    dakokuBtn = document.getElementById('dakoku_button');
-    dakokuBtn.disabled = true;
+    attendanceBtn = document.getElementById("attendance_button");
+    attendanceBtn.disabled = true;
+    // TODO: ローディング処理ほしい
+    document.getElementById("dakoku_result").textContent = `Loading...`
+    // ログイン処理
+    let data = getFormData();
+    let res = await pywebview.api.ieyasu_login(data);
+
+    if (res) {
+        // ログイン成功時
+        document.getElementById("dakoku_result").textContent = `ログイン成功`
+        localStorage.setItem('login_url', data["url"]);
+        let res = await pywebview.api.ieyasu_attendance();
+        localStorage.setItem('last_timestamp', res);
+        // 打刻状態判定
+        let dakokuStats = await pywebview.api.check_leaving(true);
+        if (dakokuStats) {
+            saveStorageUserInfo(data);
+            document.getElementById("last_timestamp").textContent = `最終打刻日時: ${localStorage.getItem('last_timestamp')}`
+            document.getElementById("dakoku_result").textContent = `出勤完了!!`
+        } else {
+            document.getElementById("dakoku_result").textContent = `[Error]: 出勤打刻に失敗しました。`
+        }
+
+    } else {
+        document.getElementById("dakoku_result").textContent = `[Error]: ログイン処理に失敗しました。`
+    }
+}
+
+const leavingDakoku = async () => {
+    // ボタン連打防止処理
+    leavingBtn = document.getElementById("leaving_button");
+    leavingBtn.disabled = true;
     // TODO: ローディング処理ほしい
     document.getElementById("dakoku_result").textContent = `Loading...`
 
     // ログイン処理
     let data = getFormData();
     let res = await pywebview.api.ieyasu_login(data);
+
     if (res) {
         // ログイン成功時
         document.getElementById("dakoku_result").textContent = `ログイン成功`
         localStorage.setItem('login_url', data["url"]);
+        let res = await pywebview.api.ieyasu_leaving();
+        localStorage.setItem('last_timestamp', res);
         // 打刻状態判定
-        let dakokuStatsBefore = dakokuStatus.none;
-        dakokuStatsBefore = await pywebview.api.check_dakoku();
-        // 退勤済みなら出勤処理
-        if (dakokuStatsBefore === dakokuStatus.leave) {
-            let res = await pywebview.api.ieyasu_attendance();
-            localStorage.setItem('last_timestamp', res);
-
-            await attendanceDakoku(data);
-            // 出勤済みなら退勤処理
-        } else if (dakokuStatsBefore === dakokuStatus.attend) {
-            let res = await pywebview.api.ieyasu_leaving();
-            localStorage.setItem('last_timestamp', res);
-            await leavingDakoku(data);
-            // 打刻状態の取得失敗時
-        } else if (dakokuStatsBefore === dakokuStatus.none) {
-            document.getElementById("dakoku_result").textContent = `[Error]: 打刻状態の取得に失敗しました。`
+        let dakokuStats = dakokuStatus.none;
+        // dakokuStats = await pywebview.api.check_leaving(true);
+        if (dakokuStats === dakokuStatus.none) {
+            saveStorageUserInfo(data);
+            document.getElementById("last_timestamp").textContent = `最終打刻日時: ${localStorage.getItem('last_timestamp')}`
+            document.getElementById("dakoku_result").textContent = `お疲れさまでした!!!`
+        } else {
+            document.getElementById("dakoku_result").textContent = `[Error]: 退勤打刻に失敗しました。`
         }
+
     } else {
         document.getElementById("dakoku_result").textContent = `[Error]: ログイン処理に失敗しました。`
-    }
-}
-
-const attendanceDakoku = async (data) => {
-    // 打刻状態判定
-    let dakokuStats = await pywebview.api.check_dakoku(true);
-    if (dakokuStats === dakokuStatus.attend) {
-        saveStorageUserInfo(data);
-        document.getElementById("last_timestamp").textContent = `最終打刻日時: ${localStorage.getItem('last_timestamp')}`
-        document.getElementById("dakoku_result").textContent = `出勤完了!!`
-    } else {
-        document.getElementById("dakoku_result").textContent = `[Error]: 出勤打刻に失敗しました。`
-    }
-}
-
-const leavingDakoku = async (data) => {
-    // 打刻状態判定
-    let dakokuStats = dakokuStatus.none;
-    dakokuStats = await pywebview.api.check_dakoku(true);
-    if (dakokuStats === dakokuStatus.none) {
-        saveStorageUserInfo(data);
-        document.getElementById("last_timestamp").textContent = `最終打刻日時: ${localStorage.getItem('last_timestamp')}`
-        document.getElementById("dakoku_result").textContent = `お疲れさまでした!!!`
-    } else {
-        document.getElementById("dakoku_result").textContent = `[Error]: 退勤打刻に失敗しました。`
     }
 }
 
